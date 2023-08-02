f434d42a5daa55c758fd77ecf0074c02df82f82a
Eduardo Alvarenga <jeasomoza@gmail.com>
2023-08-02 15:50:52 -0600

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch main # # Initial commit # # Changes to be committed: #	new file:   .env #	new file:   .env.template #	new file:   .eslintrc.js #	new file:   .gitignore #	new file:   .prettierrc #	new file:   README.md #	new file:   docker-compose.yaml #	new file:   nest-cli.json #	new file:   package.json #	new file:   src/app.module.ts #	new file:   src/main.ts #	new file:   src/product/dto/create-product.dto.ts #	new file:   src/product/dto/update-product.dto.ts #	new file:   src/product/entities/product.entity.ts #	new file:   src/product/product.controller.ts #	new file:   src/product/product.module.ts #	new file:   src/product/product.service.ts #	new file:   test/app.e2e-spec.ts #	new file:   test/jest-e2e.json #	new file:   tsconfig.build.json #	new file:   tsconfig.json #	new file:   yarn.lock

---
+++ README.md
@@ -0,0 +1,10 @@
+# Teslo API
+1. Clonar Poryecto
+2. ```yarn install```
+3. Clonar el archivo ```.env.template``` y renombrarlo a ```.env```
+4. Cambiar las variables de entorno
+5. Levantar Base de Datos
+```
+docker-compose up -d
+```
+6. Levantar: ```yarn start:dev```
---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductModule } from './product/product.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT, // + is to convert string to number
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    ProductModule,
+  ],
+})
+export class AppModule {}
